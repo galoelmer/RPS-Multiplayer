abb45a0608eb310c453977b6b58c19f0b6e4c245
Elmer C. Galo <elmer.galo@outlook.com>
2019-10-24 00:58:00 -0400

    Create a database reference to online users list and add an event listener to add and remove users connections

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,27 @@
-$(function(){
+$(function () {
+
+    // Get database reference to all connected users
+    var onlineUsersRef = database.ref("/onlineUsersList");
+
+    // '.info/connected' is a boolean value, true if users are connected and false if they are not.
+    var connectedRef = database.ref(".info/connected");
+
+    // When the users' connection state changes...
+    connectedRef.on("value", function (snapshot) {
+
+        // If they are connected..
+        if (snapshot.val()) {
+
+            // Add user to the user connections list.
+            var onlineUser = onlineUsersRef.push({
+                score: 0,
+                choice: "rock",
+                name: "Anonymous"
+            });
+
+            // Remove user from the connection list when they disconnect.
+            onlineUser.onDisconnect().remove();
+        }
+    });
 
 });
